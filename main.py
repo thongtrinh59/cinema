0bd03c00e3bb95a843a481128b992735c10bd0f6
Thomas <thongtrinh49@gmail.com>
2021-11-07 00:57:37 -0800

    change query

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy import inspect
+from sqlalchemy.orm import sessionmaker
+
 import json
 
 app = Flask(__name__)
@@ -15,6 +17,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://root:root@db/main'
 
 db = SQLAlchemy(app)
 engine = create_engine('postgresql://root:root@db/main')
+Session = sessionmaker(bind = engine)
+session = Session()
 
 # building model
 class Cinema(db.Model):
@@ -45,11 +49,86 @@ class Ticket(db.Model):
     typeticket = db.Column(db.String(50))
     seat = db.Column(db.Integer)
 
+# Get available cinemas
+@app.route('/cinema')
+def get_avai_cinema():
+    parser = reqparse.RequestParser()
+    parser.add_argument('status', type=str,  required=False)
+    parser.add_argument('name', type=str,  required=False)
+    parser.add_argument('movie_title', type=str,  required=False)
+    
+    args = parser.parse_args()
+    print(args)
+
+    if args['status'] is not None:
+        list_of_avai_cinema = []
+
+        with engine.connect() as con:
+            q_str = "SELECT * FROM cinema;"
+            rs = con.execute(q_str)
+            for _ in rs:
+                dict_result = {
+                    "id": _.id,
+                    "name": _.name,
+                    "address": _.address,
+                    "phone": _.phone,
+                    "snack": _.snack,
+                    "capacity": _.capacity
+                }
+                list_of_avai_cinema.append(dict_result)
+
+        # avai_cinema = Cinema.query.all()
+        # for _ in avai_cinema:
+        #     dict_result = {
+        #         "id": _.id,
+        #         "name": _.name,
+        #         "address": _.address,
+        #         "phone": _.phone,
+        #         "snack": _.snack,
+        #         "capacity": _.capacity
+        #     }
+        #     list_of_avai_cinema.append(dict_result)
+
+        return {'return': list_of_avai_cinema}, 200
+
+    if args['name'] is not None:
+        list_cinema_by_name = []
+        # for _ in args['name']:
+        cinema_by_name = Cinema.query.filter_by(name=args['name']).first()
+        dict_result = {
+            "id": cinema_by_name.id,
+            "name": cinema_by_name.name,
+            "address": cinema_by_name.address,
+            "phone": cinema_by_name.phone,
+            "snack": cinema_by_name.snack,
+            "capacity": cinema_by_name.capacity
+        }
+        list_cinema_by_name.append(dict_result)
+        return {'return': list_cinema_by_name}, 200
+
+    if args['movie_title'] is not None:
+        print("OKKKKKKKKKK")
+        result = []
+        with engine.connect() as con:
+            q_str = "SELECT DISTINCT(c.name) FROM movie as m,  \
+                timeslots as t, cinema as c WHERE m.id = t.mid AND \
+                c.id = t.cid AND LOWER(m.title) = LOWER('{}');".format(args['movie_title'])
+
+            rs = con.execute(q_str)
+            for row in rs:
+                result.append(row.name)
+        print(result)
+        return {'result': result}, 200
+        
+        
+
+
+
 # # Get available cinemas
 # @app.route('/cinema')
 # def get_avai_cinema():
 #     parser = reqparse.RequestParser()
-#     parser.add_argument('status', type=str,  required=True, help='Status cannot be blank')
+#     parser.add_argument('status', type=str,  required=False, help='Status cannot be blank')
     
 #     args = parser.parse_args()
 #     print(args)
@@ -72,60 +151,12 @@ class Ticket(db.Model):
 #             # print(dict_result)
 #             list_of_result.append(dict_result)
 
-#     # with engine.connect() as con:
-
-#     #     rs = con.execute('SELECT * FROM cinema;')
-
-#     #     for row in rs:
-#     #         print(row.id)
-    
-#     # list_of_result = json.dumps(list_of_result)
     
     
 #     return {'return': list_of_result}, 200
 
 
-# Get cinema information
-@app.route('/cinema')
-def get_info_cinema():
-    parser = reqparse.RequestParser()
-    parser.add_argument('name', action='append', type=str,  required=True, help='Name cannot be blank')
-    
-    args = parser.parse_args()
-    print(args)
 
-    list_of_result = []
-    # for name in args['name']:
-    #     info_cinema = Cinema.query.filter_by(name=name).first()
-    #     dict_result = {
-    #         "id": info_cinema.id,
-    #         "name": info_cinema.name,
-    #         "address": info_cinema.address,
-    #         "phone": info_cinema.phone,
-    #         "snack": info_cinema.snack,
-    #         "capacity": info_cinema.capacity
-    #     }
-    #     list_of_result.append(dict_result)
-
-    return {'result': list_of_result}, 200
-
-
-
-# hhh = db.Table('product', db.metadata, autoload=True, autoload_with=db.engine)
-
-@app.route('/')
-def index():
-    print("hello")
-
-    # user1 = Product.query.all() 
-    # user1 = db.session.query(hhh).all()
-    # for _ in user1:
-    #     print(_)
-
-@app.route('/api')
-def internal():
-    print("api call")
-    return {"message": "An id does not exist"}, 200
 
 
 
